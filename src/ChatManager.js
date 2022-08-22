fca653edecabac31c07031ca60fcb9f5de415428
Joel <joelruiz2@gmail.com>
2022-08-21 23:24:48 -0500

    fixed encoder bug, improved layout, fixed UI across the board, working on GameScreen and GameScreen Actions

--- src/ChatManager.js
+++ src/ChatManager.js
@@ -2,7 +2,14 @@ const { encode } = require('shared/util/encoder');
 const storage = require('./storage');
 const mq = require('shared/services/rabbitmq');
 
+var Filter = require('bad-words'),
+    filter = new Filter();
 
+const urlRegexSafe = require('url-regex-safe');
+const urlChecker = urlRegexSafe({ exact: true })
+const MAX_CHAT_HISTORY = 30;
+
+const regexURLCheck = new RegExp('([a-zA-Z\d]+://)?((\w+:\w+@)?([a-zA-Z\d.-]+\.[A-Za-z]{2,4})(:\d+)?(/.*)?)', 'i')
 class ChatManager {
     constructor() {
         // this.setup();
@@ -59,25 +66,37 @@ class ChatManager {
         if (!action.payload.message || typeof action.payload.message !== 'string')
             return null;
 
+
+        if (urlChecker.test(action.payload.message)) {
+            console.warn("Found URL in message: ", action.payload.message);
+            return null;
+        }
+
         let displayname = ws.user.displayname;
         let game_slug = action.payload.game_slug || undefined;
-        let message = action.payload.message;
+        let message = filter.clean(action.payload.message);
+        message = message.substring(0, 120);
         let icon = undefined;
+
+        let payload = { displayname, message, timestamp: (new Date).getTime(), };
+
         if (game_slug) {
             let game = await storage.getGameInfo(game_slug);
             if (game) {
                 icon = game.preview_images;
+                payload.game_slug = game_slug;
+                payload.icon = icon;
             }
         }
 
-        let payload = { displayname, game_slug, message, timestamp: (new Date).getTime(), icon };
+
         let response = { type: 'chat', payload }
         mq.publish('chat', 'chat', response);
 
         this.messageHistory.push(payload);
 
-        if (this.messageHistory.length > 100) {
-            this.messageHistory = this.messageHistory.slice(this.messageHistory.length - 100, this.messageHistory.length);
+        if (this.messageHistory.length > MAX_CHAT_HISTORY) {
+            this.messageHistory = this.messageHistory.slice(this.messageHistory.length - MAX_CHAT_HISTORY, this.messageHistory.length);
         }
 
         return null;
